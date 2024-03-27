f8d058c37bcbf500801338814e7672dd41c6e0d1
Antonio <aaon@cesar.school>
2024-03-27 17:22:39 -0300

    Versão inicial da tela de login

--- pet4you_app/urls.py
+++ pet4you_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "pet4you"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet")
 ]
--- pet4you_app/views.py
+++ pet4you_app/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "test1/index.html")
+    return render(request, "home.html")
 
-def greet(request, name):
-    return render(request, "test1/greet.html", {
-        "name": name.capitalize()
-    })
-# Create your views here.
