16fe3e4645affee991f02bf0a302b93325f42c22
Antonio <aaon@cesar.school>
2024-04-21 19:29:08 -0300

    Criação do model report

--- pet4you_app/models.py
+++ pet4you_app/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Pet(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=50)
     breed = models.CharField(max_length=50)
@@ -15,3 +13,12 @@ class Pet(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Report(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Report #{self.id} - {self.pet}"
