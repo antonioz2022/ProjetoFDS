1a6e43d64b2c29585bdfa00f16c68036772a5c00
Antonio <aaon@cesar.school>
2024-04-21 19:12:01 -0300

    Mudanças no login/registro

--- pet4you_app/urls.py
+++ pet4you_app/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 app_name = "pet4you"
 urlpatterns = [
-    path("signup/", views.authView, name="authView"),
     path("", views.home, name="home"),
-    path('accounts/', include("django.contrib.auth.urls")),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register_view, name='register'),
     path("posting/", views.createPost, name="posting"),
     path("list/", views.listPets, name= "list"),
     path("edit/<int:pet_id>/", views.edit_post, name="edit_post"),
--- pet4you_app/views.py
+++ pet4you_app/views.py
@@ -3,10 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
-from .models import Pet
 from django.contrib import messages
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout
+from .models import Pet
 from .forms import PetForm
 
 
@@ -32,29 +36,37 @@ def listPets(request):
     return render(request, 'list.html', {'pets': pets})
 
 
-def authView(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST or None)
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-
-            return redirect("pet4you:login")
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  
     else:
         form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
-    return render(request, "registration/signup.html", {"form": form})
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("pet4you:login")
-
-def login():
-    pass
-
-def signup():
-    pass
-
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
 
 def home(request):
     return render(request, "home.html", {})
