6bf2114954cbf265a6c8425f4dfc83153b4e6e89
tempz <tcq@cesar.school>
2024-04-29 10:12:54 -0300

    Changes

--- pet4you_app/tests.py
+++ pet4you_app/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, LiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+
+class Pet4YouTestCase(LiveServerTestCase):
+    def test_cen1(self):
+        #Dado 
+        driver.get("http://127.0.0.1:8000/pet4you_app/login/")
+        user = driver.find_element(by = By.NAME, value="username")
+        nome_user = driver.find_element(by = By.NAME, value="username")
+        senha = driver.find_element(by = By.NAME, value="password")
+        botao = driver.find_element(by = By.NAME, value="submit")
+        user.send_keys(f"tempz")
+        senha.send_keys("tempz7")
+        time.sleep(2)
+        botao.send_keys(Keys.ENTER)
+        time.sleep(5)
+
+ 
