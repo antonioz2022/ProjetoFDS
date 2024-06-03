5cd3f79a7c4c6af04c2aa3e2def4c9c8773ca538
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-06-03 19:59:06 -0300

    correção

--- cypress/e2e/notificacoes.cy.js
+++ cypress/e2e/notificacoes.cy.js
@@ -1,7 +1,7 @@
 describe('teste de notificação nas últimas 24 horas', () => {
     it('mostra a notificação de quantos pets foram adicionados nas últimas 24 horas', () => {
         cy.visit('/');
-
+        cy.get(':nth-child(1) > .nav-link').click()
         cy.get('#username').type('antonie@1234')
         cy.get('#password').type('pass@12345')
         cy.get('button').click()
