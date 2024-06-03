308bfbe7ff8279a631c7ce2d74cf1ea372eefee9
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-06-03 19:54:38 -0300

    testes de notificacao

---
+++ cypress/e2e/notificacoes.cy.js
@@ -0,0 +1,20 @@
+describe('teste de notificação nas últimas 24 horas', () => {
+    it('mostra a notificação de quantos pets foram adicionados nas últimas 24 horas', () => {
+        cy.visit('/');
+
+        cy.get('#username').type('antonie@1234')
+        cy.get('#password').type('pass@12345')
+        cy.get('button').click()
+
+        cy.wait(2000);
+
+        // Verifica se a notificação existe no DOM
+        cy.contains('Número de pets criados nas últimas 24 horas:').should('be.visible');
+
+        // Verifica se a mensagem foi registrada no console
+        cy.window().then((win) => {
+            const consoleSpy = cy.spy(win.console, 'log');
+            cy.wrap(consoleSpy).should('be.calledWithMatch', /Número de pets criados nas últimas 24 horas:/);
+        });
+    });
+});
