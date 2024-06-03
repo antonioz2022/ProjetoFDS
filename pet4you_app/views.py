88206665dd17e61d0dbb76a3e22effcf6e4a3739
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-06-03 19:44:04 -0300

    changes

--- pet4you_app/views.py
+++ pet4you_app/views.py
@@ -18,12 +18,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib.auth.models import User
 from .forms import PetFilterForm
-from winotify import Notification
 from django.utils import timezone
 from datetime import timedelta
 import platform
 if platform.system() == "Windows":
     from winotify import Notification
+    import winreg
 else:
     Notification = None
 
