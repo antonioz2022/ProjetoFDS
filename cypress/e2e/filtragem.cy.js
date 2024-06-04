ccb4d174ff6721624575dd0e6ae70b87f5cc468d
Antonio <aaon@cesar.school>
2024-06-03 22:30:45 -0300

    Correção no teste de filtragem

--- cypress/e2e/filtragem.cy.js
+++ cypress/e2e/filtragem.cy.js
@@ -6,6 +6,7 @@ describe('Pet Filtering Validation Scenarios', () => {
     cy.get('#password').type('pass@12345')
     cy.get('button').click()
     cy.get('.banner > .container > h2').invoke('text').should('have.string', "Bem-vindo ao Pet4You!")
+
   });
 
   const filterAndCheck = (inputName, inputValue, expectedText) => {
@@ -15,6 +16,19 @@ describe('Pet Filtering Validation Scenarios', () => {
   };
 
   it('filtragem por espécie com entrada do usuário', () => {
+    cy.visit('/pet4you_app/posting');
+
+    cy.get('#photo').type('https://www.petz.com.br/blog/wp-content/uploads/2021/04/raca-de-cachorro-docil-2.jpg')
+    cy.get('#name').type('teste');
+    cy.get('#species').type('dog');
+    cy.get('#breed').type('oi');
+    cy.get('#age').type('1');
+    cy.get('#description').type('Um labrador muito brincalhão!');
+
+    cy.get('.posting-submit').click();
+
+
+    cy.get('.banner > .container > h2').invoke('text').should('have.string', "Bem-vindo ao Pet4You!")
     const userInput = 'dog';
     filterAndCheck('especie', userInput, userInput);
   });
