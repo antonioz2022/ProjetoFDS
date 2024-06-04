689b196eaad331ff8a1bf7f711bf9ba79022ccbf
Vinicius <vinicahut@gmail.com>
2024-06-03 22:06:20 -0300

    teste

--- cypress/e2e/filtragem.cy.js
+++ cypress/e2e/filtragem.cy.js
@@ -2,26 +2,29 @@ describe('Pet Filtering Validation Scenarios', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-
   const filterAndCheck = (inputName, inputValue, expectedText) => {
     cy.get(`input[name="${inputName}"]`).type(inputValue);
     cy.get('input[type="submit"]').click();
-    cy.contains('div', expectedText);
+    cy.contains(expectedText);
   };
-
-  it('filtragem por espécie', () => {
-    filterAndCheck('especie', 'dog', 'dog');
+  
+  it('filtragem por espécie com entrada do usuário', () => {
+    const userInput = 'dog';
+    filterAndCheck('especie', userInput, userInput);
   });
-
-  it('filtragem por raça', () => {
-    filterAndCheck('raca', 'oi', 'oi');
+  
+  it('filtragem por raça com entrada do usuário', () => {
+    const userInput = 'oi';
+    filterAndCheck('raca', userInput, userInput);
   });
-
-  it('filtragem por nome', () => {
-    filterAndCheck('nome', 'teste', 'teste');
+  
+  it('filtragem por nome com entrada do usuário', () => {
+    const userInput = 'teste';
+    filterAndCheck('nome', userInput, userInput);
   });
-
-  it('Filtragem por idade', () => {
-    filterAndCheck('idade', '1', '1');
+  
+  it('Filtragem por idade com entrada do usuário', () => {
+    const userInput = '1';
+    filterAndCheck('idade', userInput, userInput);
   });
 });
