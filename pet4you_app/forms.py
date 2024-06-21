578eeb31e226bd65ef6c5f98e81b8000cca68461
Antonio <aaon@cesar.school>
2024-06-21 13:22:51 -0300

    Validação de erros em adicionar vacinas

--- pet4you_app/forms.py
+++ pet4you_app/forms.py
@@ -1,6 +1,8 @@
 # forms.py
 from django import forms
 from .models import Vaccine
+import datetime
+from django.core.exceptions import ValidationError
 
 class VaccineForm(forms.ModelForm):
     class Meta:
@@ -10,6 +12,18 @@ class VaccineForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
             'next_due_date': forms.DateInput(attrs={'type': 'date'}),
         }
+        
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if date > datetime.date.today():
+            raise ValidationError("A data da vacina não pode ser futura.")
+        return date
+
+    def clean_next_due_date(self):
+        next_due_date = self.cleaned_data['next_due_date']
+        if next_due_date < datetime.date.today():
+            raise ValidationError("A próxima dose não pode ser uma data passada.")
+        return next_due_date
 
 class PetFilterForm(forms.Form):
     species = forms.CharField(max_length=100, required=False)
