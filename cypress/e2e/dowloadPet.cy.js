cda522089e275ca35b08e1451b797892915f8e50
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-06-24 19:44:09 -0300

    correcoes download

--- cypress/e2e/dowloadPet.cy.js
+++ cypress/e2e/dowloadPet.cy.js
@@ -1,24 +1,30 @@
 describe('Teste de download de PDF', () => {
-    beforeEach(() => {
-      cy.visit('/');
-      cy.get(':nth-child(1) > .nav-link').click();
-      cy.get('#username').type('anton');
-      cy.get('#password').type('123456');
-      cy.get('button').click();
-    });
-  
-    it('Deve fazer o download do PDF corretamente', () => {
-      
-      cy.visit('/');
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get(':nth-child(1) > .nav-link').click();
+    cy.get('#username').type('anton');
+    cy.get('#password').type('123456');
+    cy.get('button').click();
+  });
 
-      cy.get('.services .service').first().find('.button-2[href*="download_pet"]').click();
-  
-      cy.wait(5000);
-  
-      cy.task('listDownloads').then(downloads => {
-        const downloadedPDF = downloads.find(download => download.endsWith('.pdf'));
-        expect(downloadedPDF).to.exist
+  it('Deve fazer o download do PDF corretamente', () => {
+    // Espera até que a página esteja completamente carregada
+    cy.get('.services .service').first().should('be.visible').within(() => {
+      // Clicar no link/botão de download do PDF
+      cy.get('.button-3').click();
+    });
+
+    // Esperar até que o arquivo PDF seja baixado
+    cy.waitUntil(() => {
+      return cy.task('listDownloads').then(downloads => {
+        return downloads.some(download => download.endsWith('.pdf'));
       });
+    }, { timeout: 10000, interval: 1000 }); // Espera até 10 segundos, verificando a cada 1 segundo
+
+    // Verificar se o arquivo PDF foi baixado
+    cy.task('listDownloads').then(downloads => {
+      const downloadedPDF = downloads.find(download => download.endsWith('.pdf'));
+      expect(downloadedPDF).to.exist;
     });
   });
-  
+});
